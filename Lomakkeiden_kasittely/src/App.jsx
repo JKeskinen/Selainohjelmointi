0086956663b14d6715fdf237355c95cbfb045ebd
Juho <juho.keskinen@seamk.fi.007@gmail.com>
2026-09-08 19:40:32 +0300

    typing_text_valmis

--- Lomakkeiden_kasittely/src/App.jsx
+++ Lomakkeiden_kasittely/src/App.jsx
@@ -4,6 +4,19 @@ import Note from './components/Note'
 
 const App = (props) => {
   const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState(
+    'a new note...'
+  )
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+  const addNote =(event) => {
+    event.preventDefault()
+    console.log('button clicked', event.target)
+
+
+  }
 
   return (
     <div>
@@ -13,6 +26,13 @@ const App = (props) => {
           <Note key={note.id} note={note} />
         )}
       </ul>
+      <form onSubmit = {addNote}>
+        <input 
+        value={newNote}
+        onChange={handleNoteChange} 
+        />
+        <button type="submit">save</button>
+      </form>
     </div>
   )
 }
