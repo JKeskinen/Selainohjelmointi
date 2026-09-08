87c3c9945ce6e69909326109d83c601e05d7a931
Juho <juho.keskinen@seamk.fi.007@gmail.com>
2026-09-08 19:40:39 +0300

    modified:   Lomakkeiden_kasittely/src/App.jsx

--- Lomakkeiden_kasittely/src/App.jsx
+++ Lomakkeiden_kasittely/src/App.jsx
@@ -4,25 +4,44 @@ import Note from './components/Note'
 
 const App = (props) => {
   const [notes, setNotes] = useState(props.notes)
-  const [newNote, setNewNote] = useState(
-    'a new note...'
-  )
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
+
   const handleNoteChange = (event) => {
     console.log(event.target.value)
     setNewNote(event.target.value)
   }
+
   const addNote =(event) => {
     event.preventDefault()
-    console.log('button clicked', event.target)
-
-
+    const noteObject = {
+      content: newNote,
+      important: Math.random()>0.5,
+      id: String(notes.length +1),
+    }
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
   }
+
+  const notesToShow = showAll ? notes : notes.filter(note => note.important)
+/* muuttujan tulos arvoksi asetetaan val1:n arvo jos ehto on tosi. 
+Jos ehto ei ole tosi, muuttujan tulos arvoksi tulee val2:n arvo.
+Eli jos tilan arvo showAll on epätosi, muuttuja notesToShow saa arvokseen vain ne muistiinpanot, 
+joiden important-kentän arvo on tosi. Filtteröinti tapahtuu taulukon metodilla filter: */
+
+  
+
 
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+            show {showAll ? 'important' : 'all' }
+        </button>
+      </div> 
       <ul>
-        {notes.map(note => 
+        {notesToShow.map(note => 
           <Note key={note.id} note={note} />
         )}
       </ul>
