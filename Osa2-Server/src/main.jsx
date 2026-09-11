286c99a08c528d360189a6a247a93cf38524b2f7
Juho <juho.keskinen@seamk.fi.007@gmail.com>
2026-09-11 13:16:40 +0300

    Server. TODO: Tallennus serverille

--- Osa2-Server/src/main.jsx
+++ Osa2-Server/src/main.jsx
@@ -1,5 +1,15 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import axios from 'axios'
+
+
+const promise = axios.get('http://localhost:3001/notes')
+//console.log(promise)
+
+//tapahtumakuuntelija promiselle then-metodilla
+promise.then(response => {
+  console.log(response)
+})
 
 const notes = [
   {
